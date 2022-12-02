2ca972fcabc05a31f24759f16236ed8af2b4561e
Adrian Ciołek <ciolek.adrian@protonmail.com>
2022-12-02 01:36:47 +0100

    Added scopes

---
+++ app/scopes/models.py
@@ -0,0 +1,23 @@
+import ormar
+
+from app.db import BaseMeta
+
+
+class Scope(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "scopes"
+
+    id = ormar.Integer(primary_key=True)
+    app_name = ormar.String(max_length=64)
+    value = ormar.String(max_length=120)
+    description = ormar.String(max_length=256)
+
+    def get_tuple(self):
+        return f"{self.app_name}:{self.value}", self.description
+
+    def get_dict(self):
+        _dict = {}
+        dict_index = f"{self.app_name}:{self.value}"
+        _dict[dict_index] = self.description
+        return _dict
+
