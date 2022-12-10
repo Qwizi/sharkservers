2efd131bb9cd8e9947bab6248e6fe112d47a2aea
Adrian Ciołek <ciolek.adrian@protonmail.com>
2022-12-10 22:17:17 +0100

    small optimalize imports

--- app/main.py
+++ app/main.py
@@ -1,35 +1,21 @@
-import logging
-from logging.config import dictConfig
-from typing import List
-
-import aioredis
-import uvicorn
-from fastapi import FastAPI, Depends
-from fastapi_events.dispatcher import dispatch
+from fastapi import FastAPI
 from fastapi_events.handlers.local import local_handler
 from fastapi_events.middleware import EventHandlerASGIMiddleware
-from pydantic import BaseModel
-from starlette.requests import Request
 
 from app.__version import VERSION
-from app.auth.schemas import ActivateUserCode
-from app.auth.utils import create_activate_code, activate_user
-from app.db import database, create_redis_pool, get_redis
+from app.db import database, create_redis_pool
 from app.logger import logger
 from app.roles.utils import create_default_roles
-from app.scopes.models import Scope
 from app.scopes.utils import create_scopes
-from app.settings import Settings, get_settings
-from app.users.models import User
 from fastapi_pagination import add_pagination
-from fastapi_events.typing import Event
-from app.users.schemas import UserEvents
+
 # Routes
 from app.users.views import router as users_router
 from app.auth.views import router as auth_router
 from app.scopes.views import router as scopes_router
 from app.roles.views import router as roles_router
 
+# Events
 from app.users.handlers import (
     handle_user_register_event,
     create_activate_code_after_register
