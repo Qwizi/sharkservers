5cd9a601cf509052c5858c25e01f86c1c67ad2d3
Adrian Ciołek <ciolek.adrian@protonmail.com>
2022-12-18 18:53:42 +0100

    Refactoring users admin views

--- app/users/views_admin.py
+++ app/users/views_admin.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, Security
+from fastapi_events.dispatcher import dispatch
 from fastapi_pagination import Page, Params, paginate
+from fastapi_pagination.bases import AbstractPage
 from fastapi_pagination.ext.ormar import paginate
 from ormar import NoMatch
 
@@ -7,56 +9,69 @@ from app.auth.schemas import RegisterUserSchema
 from app.auth.utils import register_user, get_current_active_user, get_admin_user
 from app.roles.exceptions import RoleNotFound
 from app.roles.models import Role
+from app.users.enums import UsersAdminEventsEnum
 from app.users.exceptions import UserNotFound
 from app.users.models import User
 from app.users.schemas import UserOutWithEmail, CreateUserSchema
+from app.users.utils import _admin_get_users, _admin_get_user, _admin_create_user, _admin_delete_user
 
 router = APIRouter()
 
 
 @router.get("", response_model=Page[UserOutWithEmail], response_model_exclude_none=True)
 async def admin_get_users(params: Params = Depends(),
-                          user: User = Security(get_admin_user, scopes=["users:get_all"])):
-    users = User.objects.select_related("display_role")
-    return await paginate(users, params)
+                          user: User = Security(get_admin_user, scopes=["users:get_all"])) -> AbstractPage:
+    """
+    Admin get all users
+    :param params:
+    :param user:
+    :return Page[UserOutWithEmail]:
+    """
+    dispatch(UsersAdminEventsEnum.GET_ALL_PRE, payload={"data": params})
+    users = await _admin_get_users(params)
+    dispatch(UsersAdminEventsEnum.GET_ALL_POST, payload={"data": users})
+    return users
 
 
 @router.get("/{user_id}", response_model=UserOutWithEmail)
-async def admin_get_user(user_id: int, user: User = Security(get_admin_user, scopes=["users:retrieve"])):
-    try:
-        user = await User.objects.select_related(["display_role", "roles", "steamprofile"]).get(id=user_id)
-    except NoMatch:
-        raise UserNotFound
+async def admin_get_user(user_id: int,
+                         user: User = Security(get_admin_user, scopes=["users:retrieve"])) -> UserOutWithEmail:
+    """
+    Admin get user
+    :param user_id:
+    :param user:
+    :return UserOutWithEmail:
+    """
+    dispatch(UsersAdminEventsEnum.GET_ONE_PRE, payload={"data": user_id})
+    user = await _admin_get_user(user_id)
+    dispatch(UsersAdminEventsEnum.GET_ONE_POST, payload={"data": user})
     return user
 
 
 @router.post("", response_model=UserOutWithEmail)
 async def admin_create_user(user_data: CreateUserSchema,
-                            user: User = Security(get_admin_user, scopes=["users:create"])):
-    register_user_schema = RegisterUserSchema(
-        username=user_data.username,
-        email=user_data.email,
-        password=user_data.password,
-        password2=user_data.password
-    )
-    created_user = await register_user(register_user_schema)
-    if user_data.is_activated:
-        await created_user.update(is_activated=True)
-    if user_data.display_role:
-        try:
-            role = await Role.objects.get(id=user_data.display_role)
-            await created_user.update(display_role=role)
-            await created_user.roles.add(role)
-        except NoMatch:
-            raise RoleNotFound()
-    return created_user
+                            user: User = Security(get_admin_user, scopes=["users:create"])) -> UserOutWithEmail:
+    """
+    Admin create user
+    :param user_data:
+    :param user:
+    :return UserOutWithEmail:
+    """
+    dispatch(UsersAdminEventsEnum.CREATE_PRE, payload={"data": user_data})
+    user = await _admin_create_user(user_data)
+    dispatch(UsersAdminEventsEnum.CREATE_POST, payload={"data": user})
+    return user
 
 
 @router.delete("/{user_id}")
-async def admin_delete_user(user_id: int, user: User = Security(get_admin_user, scopes=["users:delete"])):
-    try:
-        user = await User.objects.get(id=user_id)
-        await user.delete()
-    except NoMatch:
-        raise UserNotFound
+async def admin_delete_user(user_id: int, user: User = Security(get_admin_user, scopes=["users:delete"])) -> dict:
+    """
+    Admin delete user
+    :param user_id:
+    :param user:
+    :return dict:
+    """
+    dispatch(UsersAdminEventsEnum.DELETE_PRE, payload={"data": user_id})
+    user = await _admin_delete_user(user_id)
+    dispatch(UsersAdminEventsEnum.DELETE_POST, payload={"data": user})
     return {"msg": "Successfully deleted user"}
