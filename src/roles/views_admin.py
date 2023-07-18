e506684a267e07faea57f4ac22fb71b70ab3176c
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-07-18 19:18:09 +0200

    Updated roles tests

--- src/roles/views_admin.py
+++ src/roles/views_admin.py
@@ -1,4 +1,3 @@
-from asyncpg import UniqueViolationError
 from fastapi import APIRouter, Security, Depends
 from fastapi_events.dispatcher import dispatch
 from fastapi_pagination import Page, Params
@@ -6,12 +5,16 @@ from fastapi_pagination.bases import AbstractPage
 from ormar import NoMatch
 
 from src.auth.dependencies import get_admin_user
+from src.roles.dependencies import get_roles_service, get_valid_role
 from src.roles.enums import RolesAdminEventsEnum
-from src.roles.exceptions import role_exists_exception, role_not_found_exception
+from src.roles.exceptions import role_not_found_exception
 from src.roles.models import Role
 from src.roles.schemas import RoleOut, RoleOutWithScopes, CreateRoleSchema
-from src.roles.utils import _get_roles, _get_role, _delete_role
+from src.roles.services import RoleService
+from src.roles.utils import _delete_role
+from src.scopes.dependencies import get_scopes_service
 from src.scopes.models import Scope
+from src.scopes.services import ScopeService
 from src.users.models import User
 
 router = APIRouter()
@@ -19,73 +22,82 @@ router = APIRouter()
 
 @router.get("", response_model=Page[RoleOut])
 async def admin_get_roles(
-    params: Params = Depends(),
-    user: User = Security(get_admin_user, scopes=["roles:all"]),
+        params: Params = Depends(),
+        admin_user: User = Security(get_admin_user, scopes=["roles:all"]),
+        roles_service: RoleService = Depends(get_roles_service),
 ) -> AbstractPage[RoleOut]:
     """
     Admin get all roles.
+    :param roles_service:
     :param params:
-    :param user:
+    :param admin_user:
     :return AbstractPag[RoleOut]:
     """
     dispatch(RolesAdminEventsEnum.GET_ALL_PRE, payload={"data": params})
-    roles = await _get_roles(params)
+    roles = await roles_service.get_all(params=params)
     dispatch(RolesAdminEventsEnum.GET_ALL_POST, payload={"data": roles})
     return roles
 
 
 @router.get("/{role_id}", response_model=RoleOutWithScopes)
 async def admin_get_role(
-    role_id: int, user: User = Security(get_admin_user, scopes=["roles:retrieve"])
+        role: Role = Depends(get_valid_role), user: User = Security(get_admin_user, scopes=["roles:retrieve"])
 ) -> RoleOutWithScopes:
     """
     Admin get role by id.
+    :param role:
     :param role_id:
     :param user:
     :return:
     """
-    dispatch(RolesAdminEventsEnum.GET_ONE_PRE, payload={"data": role_id})
-    roles = await _get_role(role_id)
-    dispatch(RolesAdminEventsEnum.GET_ONE_POST, payload={"data": roles})
-    return roles
+    dispatch(RolesAdminEventsEnum.GET_ONE_PRE, payload={"data": role.id})
+    dispatch(RolesAdminEventsEnum.GET_ONE_POST, payload={"data": role})
+    return role
 
 
 @router.post("", response_model=RoleOutWithScopes)
 async def admin_create_role(
-    role_data: CreateRoleSchema,
-    user: User = Security(get_admin_user, scopes=["roles:create"]),
+        role_data: CreateRoleSchema,
+        admin_user: User = Security(get_admin_user, scopes=["roles:create"]),
+        roles_service: RoleService = Depends(get_roles_service),
+        scopes_service: ScopeService = Depends(get_scopes_service),
 ):
-    scopes = None
-    if role_data.scopes:
-        scopes = await Scope.objects.filter(id__in=role_data.scopes).all()
-        print(scopes)
-    try:
-        role = await Role.objects.create(
-            name=role_data.name, color=role_data.color, is_staff=role_data.is_staff
-        )
-        if scopes:
-            for scope in scopes:
-                await role.scopes.add(scope)
-        return role
-    except UniqueViolationError as e:
-        raise role_exists_exception
+    """
+    Admin create role.
+    :param role_data:
+    :param admin_user:
+    :param roles_service:
+    :param scopes_service:
+    :return:
+    """
+    dispatch(RolesAdminEventsEnum.CREATE_PRE, payload={"data": role_data})
+    new_role = await roles_service.admin_create_role(role_data, scopes_service)
+    dispatch(RolesAdminEventsEnum.CREATE_POST, payload={"data": new_role})
+    return new_role
 
 
 @router.delete("/{role_id}")
 async def admin_delete_role(
-    role_id: int, user: User = Security(get_admin_user, scopes=["roles:delete"])
+        role: Role = Depends(get_valid_role), user: User = Security(get_admin_user, scopes=["roles:delete"]),
+        roles_service: RoleService = Depends(get_roles_service),
 ):
-    dispatch(RolesAdminEventsEnum.DELETE_PRE, payload={"data": role_id})
-    role = await _delete_role(role_id)
+    """
+    Admin delete role.
+    :param role:
+    :param user:
+    :return:
+    """
+    dispatch(RolesAdminEventsEnum.DELETE_PRE, payload={"data": role.id})
+    await roles_service.delete(_id=role.id)
     dispatch(RolesAdminEventsEnum.DELETE_POST, payload={"data": role})
     return role
 
 
 @router.post("/{role_id}/scopes/add", response_model=RoleOut)
 async def admin_add_scopes_to_role(
-    role_id: int,
-    scopes: list[int],
-    user: User = Security(get_admin_user, scopes=["roles:create"]),
+        role_id: int,
+        scopes: list[int],
+        user: User = Security(get_admin_user, scopes=["roles:create"]),
 ):
     role = await Role.objects.select_related(["scopes"]).get(id=role_id)
     if not role:
--- tests/roles_test.py
+++ tests/roles_test.py
@@ -1,112 +1,49 @@
 import pytest
 
-from src.roles.models import Role
-from src.roles.utils import (
-    create_default_roles,
-    get_admin_role_scopes,
-    get_user_role_scopes,
-)
-from src.scopes.models import Scope
-from src.scopes.utils import create_scopes
-from tests.conftest import create_fake_roles
+from src.roles.enums import ProtectedDefaultRolesEnum
+from src.scopes.dependencies import get_scopes_service
+from src.scopes.services import ScopeService
+
+ROLES_ENDPOINT = "/v1/roles"
 
 
 @pytest.mark.asyncio
-async def test_get_default_roles(client):
-    await create_default_roles()
-    r = await client.get("roles")
+async def test_get_roles(client):
+    r = await client.get(ROLES_ENDPOINT)
     assert r.status_code == 200
-    data = r.json()
-    assert len(data["items"]) == 3
-    assert data["items"][0]["id"] == 1
-    assert data["items"][1]["id"] == 2
-    assert data["items"][2]["id"] == 3
+    # 3 default roles [Admin, User, Banned]
+    assert r.json()["total"] == 3
 
 
 @pytest.mark.asyncio
-async def test_get_roles(client, faker):
-    await create_default_roles()
-    roles = await create_fake_roles(faker, 100)
-    r = await client.get("roles")
+@pytest.mark.parametrize("default_roles_id", [
+    ProtectedDefaultRolesEnum.ADMIN.value,
+    ProtectedDefaultRolesEnum.USER.value,
+    ProtectedDefaultRolesEnum.BANNED.value,
+])
+async def test_get_default_role(default_roles_id, client):
+    r = await client.get(f"{ROLES_ENDPOINT}/{default_roles_id}")
     assert r.status_code == 200
-    data = r.json()
-    assert data["total"] == 100 + 3
+    assert r.json()["id"] == default_roles_id
+
+    scopes_service: ScopeService = await get_scopes_service()
+    if default_roles_id == ProtectedDefaultRolesEnum.ADMIN.value:
+        admin_scopes = await scopes_service.get_default_scopes_for_role(ProtectedDefaultRolesEnum.ADMIN.value)
+        assert r.json()["name"] == "Admin"
+        assert r.json()["color"] == "#C53030"
+        assert len(r.json()["scopes"]) == len(admin_scopes)
+    elif default_roles_id == ProtectedDefaultRolesEnum.USER.value:
+        user_roles = await scopes_service.get_default_scopes_for_role(ProtectedDefaultRolesEnum.USER.value)
+        assert r.json()["name"] == "User"
+        assert r.json()["color"] == "#99999"
+        assert len(r.json()["scopes"]) == len(user_roles)
+    elif default_roles_id == ProtectedDefaultRolesEnum.BANNED.value:
+        assert r.json()["name"] == "Banned"
+        assert r.json()["color"] == "#000000"
+        assert len(r.json()["scopes"]) == 0
 
 
 @pytest.mark.asyncio
-async def test_get_role(client, faker):
-    await create_default_roles()
-    roles = await create_fake_roles(faker, 100)
-    role = roles[2]
-    r = await client.get(f"roles/{role.id}")
-    assert r.status_code == 200
-    data = r.json()
-    assert data["id"] == role.id
-    user_scopes = await get_user_role_scopes()
-    assert len(data["scopes"]) == len(user_scopes)
-
-
-@pytest.mark.asyncio
-async def test_get_invalid_id_role(client):
-    await create_default_roles()
-    r = await client.get("roles/9555")
+async def test_get_invalid_role(client):
+    r = await client.get(f"{ROLES_ENDPOINT}/4")
     assert r.status_code == 404
-    assert r.json()["detail"] == "Role not found"
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("role_id", [1, 2, 3])
-async def test_get_default_role(role_id, client):
-    await create_default_roles()
-    r = await client.get(f"roles/{role_id}")
-    assert r.status_code == 200
-    data = r.json()
-    assert data["id"] == role_id
-
-
-@pytest.mark.asyncio
-async def test_get_admin_role(client):
-    admin_role_id = 1
-    await create_scopes()
-    await create_default_roles()
-    r = await client.get(f"roles/{admin_role_id}")
-    assert r.status_code == 200
-    data = r.json()
-    admin_scopes = await get_admin_role_scopes()
-    assert len(data["scopes"]) == len(admin_scopes)
-
-
-@pytest.mark.asyncio
-async def test_get_user_role(client):
-    user_role_id = 2
-    await create_scopes()
-    await create_default_roles()
-    r = await client.get(f"roles/{user_role_id}")
-    assert r.status_code == 200
-    data = r.json()
-    users_scopes = await get_user_role_scopes()
-    assert len(data["scopes"]) == len(users_scopes)
-
-
-@pytest.mark.asyncio
-async def test_get_banned_role(client):
-    banned_role_id = 3
-    await create_scopes()
-    await create_default_roles()
-    r = await client.get(f"roles/{banned_role_id}")
-    assert r.status_code == 200
-    data = r.json()
-    assert len(data["scopes"]) == 0
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_get_staff_roles(client):
-    await create_scopes()
-    await create_default_roles()
-    r = await client.get("roles/staff")
-    assert r.status_code == 200
-
-    data = r.json()
-
-    assert data["items"][0]["id"] == 1
