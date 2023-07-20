d69290303751ff3d3c7e590f518da5541d2478db
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-07-20 18:59:44 +0200

    Updated scopes tests

--- tests/scopes_test.py
+++ tests/scopes_test.py
@@ -1,40 +1,29 @@
 import pytest
-from starlette import status
 
-from src.roles.utils import create_default_roles
-from src.scopes.models import Scope
-from src.scopes.utils import create_scopes
+from src.roles.enums import ProtectedDefaultRolesEnum
 
-
-@pytest.mark.asyncio
-async def test_scopes_list(client, faker):
-    await create_scopes()
-    r = await client.get("/scopes")
-    assert r.status_code == status.HTTP_200_OK
-    data = r.json()
-    assert "app_name" in data["items"][0]
-    item_found = False
-    for item in data["items"]:
-        if item["app_name"] == "users" and item["value"] == "me":
-            item_found = True
-            break
-    assert item_found is True
-    scopes = await Scope.objects.all()
-    assert len(scopes) == data["total"]
+SCOPES_ENDPOINT = "/v1/scopes"
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("role_id", [1, 2])
-async def test_scopes_list_with_role_id(role_id, client):
-    await create_scopes()
-    await create_default_roles()
-    r = await client.get(f"/scopes?role_id={role_id}")
+async def test_get_all_scopes(client):
+    r = await client.get(SCOPES_ENDPOINT)
     assert r.status_code == 200
 
 
 @pytest.mark.asyncio
-async def test_scopes_list_with_invalid_role_id(client):
-    await create_scopes()
-    r = await client.get("/scopes?role_id=-1")
+@pytest.mark.parametrize("role_id", [
+    ProtectedDefaultRolesEnum.ADMIN.value,
+    ProtectedDefaultRolesEnum.USER.value,
+    ProtectedDefaultRolesEnum.BANNED.value,
+])
+async def test_get_scopes_by_role_id(role_id, client):
+    r = await client.get(SCOPES_ENDPOINT + f"?role_id={role_id}")
+    assert r.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_get_scopes_by_role_id_not_found(client):
+    r = await client.get(SCOPES_ENDPOINT + f"?role_id=999999")
     assert r.status_code == 200
     assert r.json()["total"] == 0
