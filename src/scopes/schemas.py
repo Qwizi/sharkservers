b54b7063abb201315299e097e7827fe7b78fea06
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-07-21 15:17:43 +0200

    Updated scopes admin tests

--- src/scopes/schemas.py
+++ src/scopes/schemas.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 from src.scopes.models import Scope
@@ -10,3 +12,10 @@ class CreateScopeSchema(BaseModel):
     value: str
     description: str
     protected: bool = True
+
+
+class UpdateScopeSchema(BaseModel):
+    app_name: Optional[str]
+    value: Optional[str]
+    description: Optional[str]
+    protected: Optional[bool]
