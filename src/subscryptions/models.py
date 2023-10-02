97cf7d4c48f6ad8c8ad2d883878da555db81971c
Adrian Ciołek <ciolek.adrian@protonmail.com>
2023-10-02 19:10:27 +0200

    Updated stripe

--- src/subscryptions/models.py
+++ src/subscryptions/models.py
@@ -2,6 +2,7 @@ import datetime
 from enum import unique
 import uuid
 import ormar
+from src.roles.models import Role
 
 from src.db import BaseMeta, DateFieldsMixins
 from src.users.models import User
@@ -13,6 +14,7 @@ class UserSubscription(ormar.Model, DateFieldsMixins):
     id: str = ormar.UUID(primary_key=True, default=uuid.uuid4)
     user: User = ormar.ForeignKey(User)
     stripe_customer_id: str = ormar.String(unique=True, max_length=255)
-    stripe_subscription_id: str = ormar.String(unique=True, max_length=255)
+    stripe_subscription_id: str = ormar.String(unique=True, max_length=255, nullable=True)
     stripe_price_id: str = ormar.String(max_length=255)
-    stripe_current_period_end: datetime.datetime = ormar.DateTime()
+    stripe_current_period_end: datetime.datetime = ormar.DateTime(timezone=False)
+    old_display_role: Role = ormar.ForeignKey(Role)
